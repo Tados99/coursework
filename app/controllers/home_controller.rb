2a863e7ea938f7d6a944e20b97ee58635cf3147f
Tados99 <csanakia@gmail.com>
2020-11-19 15:04:40 +0000

    NavBar is corrected

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,31 +2,7 @@ class HomeController < ApplicationController
   def home
   end
 
-  def fixture_setup
-  end
-
-  def fixtures
-  end
-
-  def player_profile
-  end
-
-  def player_setup
-  end
-
-  def setup
-  end
-
   def standings
-  end
-
-  def team_setup
-  end
-
-  def team
-  end
-
-  def teams
   end
 
   def contact
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   resources :teams
   resources :fixtures
   root 'home#home'
+  post 'request_contact', to: 'home#request_contact'
   get 'home/standings'
   get 'home/contact'
-  post 'request_contact', to: 'home#request_contact'
+  get 'fixtures#show'
+  get 'players#show'
+  get 'team#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
