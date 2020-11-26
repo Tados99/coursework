3d4dbe3bc99e3361e12a3d875f41492509037d9c
Tados99 <csanakia@gmail.com>
2020-11-26 18:22:44 +0000

    Added extra tests

--- test/models/fixture_test.rb
+++ test/models/fixture_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class FixtureTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @team = teams(:one)
+    @team2 = teams(:two)
+  end
+
+  test 'should not save empty fixture' do
+    fixture = Fixture.new
+
+    fixture.save
+    refute fixture.valid?
+  end
+
+  test "should save valid fixture" do
+    fixture = Fixture.new
+
+    fixture.home_team_id = @team.id
+    fixture.away_team_id = @team2.id
+
+    fixture.save
+    assert fixture.valid?
+  end
 end
